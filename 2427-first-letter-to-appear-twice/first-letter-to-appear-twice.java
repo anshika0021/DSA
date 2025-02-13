8fba9c7a5f234bb0e5a46d5d7b93aeaa0bffac13
Anshika Sharma <157565294+anshika0021@users.noreply.github.com>
2025-02-13 19:25:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (43.46%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        HashSet<Character> set = new HashSet<>();
+
+        for(char ch : s.toCharArray()){
+            if(set.contains(ch)){
+                return ch;
+            }
+            else{
+                set.add(ch);
+            }
+        }
+        return 'c';
+    }
+    
+}
