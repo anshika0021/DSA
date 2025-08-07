6c41ca9d734429053b42f3d206a4f96abb5b232a
Anshika Sharma <157565294+anshika0021@users.noreply.github.com>
2025-08-07 17:55:05 +0530

    Time: 4 ms (84.22%) | Memory: 44.7 MB (94.24%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -4,26 +4,20 @@ public class Solution {
         StringBuilder[] rows = new StringBuilder[numRows];
         for (int i = 0; i < numRows; i++) {
             rows[i] = new StringBuilder();
-        }
-        
+        }     
         int curRow = 0;
         boolean goingDown = false;
-
         for (char c : s.toCharArray()) {
             rows[curRow].append(c);
-
             if (curRow == 0 || curRow == numRows - 1) {
                 goingDown = !goingDown;
             }
-
             curRow += goingDown ? 1 : -1;
         }
-
         StringBuilder result = new StringBuilder();
         for (StringBuilder row : rows) {
             result.append(row);
         }
-
         return result.toString();
     }
 }
