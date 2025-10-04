4e82b91881bcbd67ea731fd6d1beb2404267eda7
Anshika Sharma <157565294+anshika0021@users.noreply.github.com>
2025-10-04 17:55:59 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (70.84%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -10,9 +10,8 @@ class Solution {
     private void dfs(int[][] image,int sr,int sc,int originalcolor,int color){
         if(sr<0|| sr>=image.length || sc<0||sc>=image[0].length ||image[sr][sc] != originalcolor){
             return;
-        }
-        image[sr][sc] = color;
-        
+        }     
+        image[sr][sc] = color;   
         dfs(image,sr-1,sc,originalcolor,color);
         dfs(image,sr+1,sc,originalcolor,color);
         dfs(image,sr,sc+1,originalcolor,color);
