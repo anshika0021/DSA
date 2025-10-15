3a24ae88bf446b8bd7836d36224f80147cf40595
Anshika Sharma <157565294+anshika0021@users.noreply.github.com>
2025-10-15 11:44:01 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (36.46%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -1,33 +1,33 @@
 class Solution {
-    public TreeNode findMin(TreeNode root) {
-        while (root.left != null) {
-            root = root.left;
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null){
+            return root;
+        }
+        if(key<root.val){
+            root.left = deleteNode(root.left,key);
+        }
+        else if(root.val<key){
+            root.right = deleteNode(root.right,key);
+        }
+        else{
+            if(root.left == null){
+                return root.right;
+            }
+            else if(root.right == null){
+                return root.left;
+            }
+            root.val = minval(root.right);
+            root.right = deleteNode(root.right,root.val);
+
         }
         return root;
     }
-    public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) return null;
-        if (key < root.val) {
-            root.left = deleteNode(root.left, key);
-        } else if (key > root.val) {
-            root.right = deleteNode(root.right, key);
-        } 
-        else {
-            if (root.left == null && root.right == null) {
-                return null;
-            }
-
-            else if (root.left == null) {
-                return root.right;
-            } else if (root.right == null) {
-                return root.left;
-            }
-            else {
-                TreeNode minRight = findMin(root.right);
-                root.val = minRight.val; 
-                root.right = deleteNode(root.right, minRight.val);
-            }
+    public static int minval(TreeNode root){
+        int minv = root.val;
+        while(root.left != null){
+            minv = root.left.val;
+            root = root.left;
         }
-        return root;
+        return minv;
     }
 }
