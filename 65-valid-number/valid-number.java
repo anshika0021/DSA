1bb50409abcc9f08cf0330c8940610c6d4afc1a8
Anshika Sharma <157565294+anshika0021@users.noreply.github.com>
2025-10-17 23:25:08 +0530

    Time: 11 ms (9.54%) | Memory: 45.9 MB (5.67%) - LeetSync

--- 65-valid-number/valid-number.java
+++ 65-valid-number/valid-number.java
@@ -1,12 +1,9 @@
 public class Solution {
     public boolean isNumber(String s) {
-        // Trim leading and trailing spaces
         s = s.trim();
 
-        // Regex for valid number (case-insensitive 'e')
         String regex = "^[+-]?((\\d+\\.\\d*)|(\\d*\\.\\d+)|(\\d+))([eE][+-]?\\d+)?$";
 
-        // Return whether string matches regex
         return s.matches(regex);
     }
 }
